829c5df8ba8886c2164ca6bde086814af3473ca1
WalkerSumida <walkersumida@gmail.com>
2016-01-07 14:07:20 +0900

    上書きではなく、追記に変更

--- set/index.php
+++ set/index.php
@@ -3,7 +3,7 @@ header("Content-type: text/plain; charset=UTF-8");
 if (isset($_GET['set_val']))
 {
   $state = $_GET['set_val'];
-  $fp = fopen("../storage.txt", "w");
+  $fp = fopen("../storage.txt", "a");
   fwrite($fp, $state);
   fclose($fp);
 }
