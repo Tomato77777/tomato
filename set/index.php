1ae3e37cb85a9d799cc020bd54a4d2a0d35dfabd
WalkerSumida <walkersumida@gmail.com>
2016-01-07 14:05:25 +0900

    スペルミス

--- set/index.php
+++ set/index.php
@@ -3,7 +3,7 @@ header("Content-type: text/plain; charset=UTF-8");
 if (isset($_GET['set_val']))
 {
   $state = $_GET['set_val'];
-  $fp = fopen("../storege.txt", "w");
+  $fp = fopen("../storage.txt", "w");
   fwrite($fp, $state);
   fclose($fp);
 }
