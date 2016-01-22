a7ce11c3a3fd033479dd2a9c89b359bf4dcf8cfa
Tomato77777 <otenkitoshiki7@gmail.com>
2016-01-22 10:00:56 +0900

    Create index.php

---
+++ setsensor/index.php
@@ -0,0 +1,10 @@
+<?php
+header("Content-type: text/plain; charset=UTF-8");
+if (isset($_GET['set_val']))
+{
+  $val = $_GET['set_val'] . ",";
+  $fp = fopen("../storagesensor.txt", "a");
+  fwrite($fp, $val);
+  fclose($fp);
+}
+?>
