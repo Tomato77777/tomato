0f82f1f97bee53d6e8c8f7e106c2a78f197be39e
Tomato77777 <otenkitoshiki7@gmail.com>
2016-01-26 15:20:17 +0900

    Update index.php

--- setcount/index.php
+++ setcount/index.php
@@ -2,7 +2,7 @@
 2 header("Content-type: text/plain; charset=UTF-8"); 
 3 if (isset($_GET['set_val'])) 
 4 { 
-5   $val = $_GET['set_val'] . ""; 
+5   $val = $_GET['set_val'] . ","; 
 6   $fp = fopen("../storagscount.txt", "w+"); 
 7   fwrite($fp, $val); 
 8   fclose($fp); 
