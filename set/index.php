917590c31ef6b258ce39ea950f80feab75787ece
Tomato77777 <otenkitoshiki7@gmail.com>
2016-01-27 16:59:38 +0900

    上書き

--- set/index.php
+++ set/index.php
@@ -3,7 +3,7 @@ header("Content-type: text/plain; charset=UTF-8");
 if (isset($_GET['set_val']))
 {
   $val = $_GET['set_val'] . ",";
-  $fp = fopen("../storage.txt", "a");
+  $fp = fopen("../storage.txt", "w+");
   fwrite($fp, $val);
   fclose($fp);
 }
