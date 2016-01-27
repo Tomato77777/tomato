9523aad4a5e72a6c20b64bff554071b65b21168e
Tomato77777 <otenkitoshiki7@gmail.com>
2016-01-27 12:51:21 +0900

    カウントセット

--- setcount/index.php
+++ setcount/index.php
@@ -1,11 +1,11 @@
 <?php 
-2 header("Content-type: text/plain; charset=UTF-8"); 
-3 if (isset($_GET['set_val'])) 
-4 { 
-5   $val = $_GET['set_val'] . "<br />\n"; 
-6   $fp = fopen("../storagecount.txt", "w+"); 
-7   fwrite($fp, $val); 
-8   fclose($fp); 
-9 } 
-10 ?> 
+ header("Content-type: text/plain; charset=UTF-8"); 
+ if (isset($_GET['set_val'])) 
+ { 
+   $val = $_GET['set_val'] . "<br />\n"; 
+   $fp = fopen("../storagecount.txt", "w+"); 
+   fwrite($fp, $val); 
+   fclose($fp); 
+ } 
+ ?> 
 
