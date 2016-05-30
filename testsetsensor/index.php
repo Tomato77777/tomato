44599faa7a503c14f4296a6b4516fdf53cb49984
Tomato77777 <otenkitoshiki7@gmail.com>
2016-05-30 22:43:24 +0900

    Update index.php

--- testsetsensor/index.php
+++ testsetsensor/index.php
@@ -2,7 +2,7 @@
 header("Content-type: text/plain; charset=UTF-8"); 
 if (isset($_GET['set_val'])) 
 { 
- $val = date('Y年m月d日H+1時i分').$_GET['set_val'] . "<br />\n"; 
+ $val = date('Y-m-d-h/i').$_GET['set_val'] . "<br />\n"; 
  $fp = fopen("../teststoragesensor.txt", "a"); 
  fwrite($fp, $val); 
  fclose($fp); 
