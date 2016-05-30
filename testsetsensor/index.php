5adea6f0eda30be1e8c38507bc75b365be62e9da
Tomato77777 <otenkitoshiki7@gmail.com>
2016-05-30 22:34:17 +0900

    Update index.php

--- testsetsensor/index.php
+++ testsetsensor/index.php
@@ -4,7 +4,7 @@ if (isset($_GET['set_val']))
 { 
  $val = $_GET['set_val'] . "<br />\n"; 
  $fp = fopen("../teststoragesensor.txt", "a"); 
- fwrite($fp, $val); 
+ fwrite($fp, date('Y m d'),$val); 
  fclose($fp); 
 } 
 ?> 
