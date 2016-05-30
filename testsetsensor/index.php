cd1a686ceba242014343e29c432957800fe4166d
Tomato77777 <otenkitoshiki7@gmail.com>
2016-05-30 22:38:31 +0900

    Update index.php

--- testsetsensor/index.php
+++ testsetsensor/index.php
@@ -2,8 +2,7 @@
 header("Content-type: text/plain; charset=UTF-8"); 
 if (isset($_GET['set_val'])) 
 { 
- $val = date('Y m d');
- $val = $_GET['set_val'] . "<br />\n"; 
+ $val = date('Y m d').$_GET['set_val'] . "<br />\n"; 
  $fp = fopen("../teststoragesensor.txt", "a"); 
  fwrite($fp, $val); 
  fclose($fp); 
