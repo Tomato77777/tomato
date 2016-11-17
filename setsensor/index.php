811604ca9a937af56181e2e7733985de3cb5f115
Tomato77777 <otenkitoshiki7@gmail.com>
2016-11-17 14:22:41 +0900

    Update index.php

--- setsensor/index.php
+++ setsensor/index.php
@@ -1,5 +1,7 @@
 <?php
 header("Content-type: text/plain; charset=UTF-8");
+date_default_timezone_set('Asia/Tokyo');
+
 if (isset($_GET['set_val']))
 {
   $val = date("Y/m/d H:i//").$_GET['set_val'] . "<br />\n";
@@ -8,3 +10,4 @@ if (isset($_GET['set_val']))
   fclose($fp);
 }
 ?>
+
