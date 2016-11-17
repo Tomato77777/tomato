28380f038093f8fb81c9dd64f139dafce2cc8c78
Tomato77777 <otenkitoshiki7@gmail.com>
2016-11-17 10:06:24 +0900

    Update index.php

--- setsensor/index.php
+++ setsensor/index.php
@@ -3,7 +3,7 @@ header("Content-type: text/plain; charset=UTF-8");
 if (isset($_GET['set_val']))
 {
   $file_name = "../storagesensor.txt";
-  $val = date("Y/m/d H:i//").$_GET['set_val'] . "℃<br />\n";
+  $val = date("Y/m/d H:i//").$_GET['set_val'] . "℃";
   $contents = file_get_contents($file_name);
   $contents = $val . "<br />\n" . $contents;
   $re = file_put_contents($file_name, $contents);
