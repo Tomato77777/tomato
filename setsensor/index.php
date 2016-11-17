12574ea6c1db59f88ee77c4079d7cdcc4a9abb76
Tomato77777 <otenkitoshiki7@gmail.com>
2016-11-17 14:16:12 +0900

    Update index.php

--- setsensor/index.php
+++ setsensor/index.php
@@ -1,10 +1,13 @@
 <?php
-header("Content-type: text/plain; charset=UTF-8");
+date_default_timezone_set('Asia/Tokyo');
+
 if (isset($_GET['set_val']))
 {
-  $val = date("Y/m/d H:i//").$_GET['set_val'] . "<br />\n";
-  $fp = fopen("../storagesensor.txt", "a");
-  fwrite($fp, $val);
-  fclose($fp);
+  $file_name = "../storagesensor.txt";
+  $set_val = $_GET['set_val']
+  $val = date("Y/m/d H:i//").$set_val . "℃";
+  $contents = file_get_contents($file_name);
+  $contents = $val . "<br />\n" . $contents;
+  $re = file_put_contents($file_name, $contents);
 }
 ?>
