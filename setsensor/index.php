2dfb273e06466259c4f9e6f82606106a90ee8ebb
Tomato77777 <otenkitoshiki7@gmail.com>
2016-11-30 22:11:36 +0900

    ℃追加

--- setsensor/index.php
+++ setsensor/index.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Tokyo');
 if (isset($_GET['set_val']))
 {
   $file_name = "../storagesensor.txt";
-  $val = date("Y/m/d H:i//").$_GET['set_val'].'℃';
+  $val = date("Y/m/d H:i//").$_GET['set_val'].'℃です！';
   $contents = file_get_contents($file_name);
   $contents = $val . "<br />\n" . $contents;
   file_put_contents($file_name, $contents);
