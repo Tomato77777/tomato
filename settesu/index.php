6219b14c3185d8e5ab6033e2b8d50d1bd48aeea5
Tomato77777 <otenkitoshiki7@gmail.com>
2017-12-02 11:58:18 +0900

    Update index.php

--- settesu/index.php
+++ settesu/index.php
@@ -1,9 +1,9 @@
 <?php
 header("Content-type: text/plain; charset=UTF-8");
-if (isset($_GET['set_val1']))
+if (isset($_GET['set_val']))
 { 
   $val = $_GET['set_val'] . ",";
-  $fp = fopen("../storage1.txt", "w+");
+  $fp = fopen("../storage.txt", "w+");
   fwrite($fp, $val);
   fclose($fp);
  }
