26ce44ea10c029d88b5da5ab5cf1b38794e49e08
Tomato77777 <otenkitoshiki7@gmail.com>
2017-12-02 22:47:56 +0900

    実験用センサー情報リセット

--- testresetsensor/index.php
+++ testresetsensor/index.php
@@ -1,13 +1,7 @@
 <?php 
-header("Content-type: text/plain; charset=UTF-8"); 
-date_default_timezone_set('Asia/Tokyo');
-
-if (isset($_GET['set_val']))
-{
-  $file_name = "../teststoragesensor.txt";
-  $val = date("Y/m/d H:i//").$_GET['set_val'].'℃です！';
-  $contents = file_get_contents($file_name);
-  $contents = $val . "<br />\n" . $contents;
-  file_put_contents($file_name, $contents);
-}
+ader("Content-type: text/plain; charset=UTF-8");
+$val = "情報なし";
+$fp = fopen("../storagesensor.txt", "w");
+fwrite($fp, $val);
+fclose($fp);
 ?>
