a326653aecb7485618cd671e7bc8f051cb055710
Tomato77777 <otenkitoshiki7@gmail.com>
2017-12-02 22:49:22 +0900

    実験用情報リセット

--- testresetsensor/index.php
+++ testresetsensor/index.php
@@ -1,5 +1,5 @@
 <?php 
-ader("Content-type: text/plain; charset=UTF-8");
+header("Content-type: text/plain; charset=UTF-8");
 $val = "情報なし";
 $fp = fopen("../storagesensor.txt", "w");
 fwrite($fp, $val);
